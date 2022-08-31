6e4d3f9748292d0a3be786ee861d6a39cebe2369
riccardo.campisano <riccardo.campisano@gmail.com>
2022-08-31 23:57:00 +0200

    fix:        CI - fix dockerfile build arguments, install binfmt arm64 qemu emulator

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -21,11 +21,14 @@ git push origin tag ${RELEASE_TAG}
 docker pull "${DOCKER_REPOSITORY}:${RELEASE_TAG}" &> /dev/null && echo "ERROR: docker image \"${DOCKER_REPOSITORY}:${RELEASE_TAG}\" already exists" && exit 1
 
 # build docker image
+docker pull tonistiigi/binfmt:latest
+docker run --privileged --rm tonistiigi/binfmt --uninstall qemu-*
+docker run --privileged --rm tonistiigi/binfmt --install arm64
 docker buildx ls
 docker buildx create --use
 docker buildx build --push --platform=linux/amd64,linux/arm64/v8  \
-       --env BUILD_DIR_AMD64 \
-       --env BUILD_DIR_ARM64V8 \
+       --build-arg "BUILD_DIR_AMD64=${BUILD_DIR_AMD64=}" \
+       --build-arg "BUILD_DIR_ARM64V8=${BUILD_DIR_ARM64V8=}" \
        --build-arg "FROM_IMAGE=${DOCKER_IMAGE_FROM=}" \
        --tag "${DOCKER_REPOSITORY}:${RELEASE_TAG}" \
        --tag "${DOCKER_REPOSITORY}:${PROJECT_NAME}-latest" \
